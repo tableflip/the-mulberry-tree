1eee817a47c76fc5f7adb5643e4f7eb1c3e90c49
Alan Shaw <alan@tableflip.io>
2016-06-28 23:08:26 +0100

    Add mobile nav

--- pages/main.js
+++ pages/main.js
@@ -1,2 +1,20 @@
 require('svg4everybody')()
-window.$ = window.jQuery = require('jquery')
+// window.$ = window.jQuery = require('jquery')
+
+var navToggler = document.querySelector('.navbar-toggler')
+var navTogglerTarget = document.querySelector(navToggler.getAttribute('data-target'))
+
+navToggler.addEventListener('click', function (e) {
+  e.preventDefault()
+
+  if (navToggler.classList.contains('collapsed')) {
+    navToggler.innerHTML = '&#9776;'
+    navTogglerTarget.setAttribute('aria-expanded', 'false')
+  } else {
+    navToggler.innerHTML = '&times;'
+    navTogglerTarget.setAttribute('aria-expanded', 'true')
+  }
+
+  navToggler.classList.toggle('collapsed')
+  navTogglerTarget.classList.toggle('in')
+})
